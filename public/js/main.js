d1396bc62944661dfcbfef73d07adc213042d29b
Brian Mulwa <mw711990838@gmail.com>
2025-04-29 18:31:05 +0300

    Cleaned UI
    Clean UI, Home and Products

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     const mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.classList.toggle('active'); // Toggle the 'active' class
 }
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+});
--- routes/Web/webProductRoute.js
+++ routes/Web/webProductRoute.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const { createProduct, getAProduct, getAllProducts, updateProduct, deleteAProduct, getAllProductsSorted } = require("../../controller/productCtrl");
+const {
+  createProduct,
+  getAProduct,
+  getAllProducts,
+  updateProduct,
+  deleteAProduct,
+  getAllProductsSorted,
+} = require("../../controller/productCtrl");
 const router = express.Router();
 const { authMiddleware, isAdmin } = require("../../middlewares/authMiddleware");
 
@@ -7,45 +14,46 @@ const { authMiddleware, isAdmin } = require("../../middlewares/authMiddleware");
 /* router.post("/", authMiddleware, isAdmin, createProduct);//create a new product */
 
 //router.get("/", getAllProducts);//get all products as JSON, for web home
-router.get("/dev", getAllProducts);//get all products, no fields ommited: for development purposes
+router.get("/dev", getAllProducts); //get all products, no fields ommited: for development purposes
 /**router.get("/products/", getAllProductsSorted);//get all products and sort accordingly */
-router.get("/products/:id", getAProduct);//get a product
-router.put("/products/:id", authMiddleware, isAdmin, updateProduct);//update a product
+router.get("/products/:id", getAProduct); //get a product
+router.put("/products/:id", authMiddleware, isAdmin, updateProduct); //update a product
 // web user does not need delete yet: router.delete("/:id", authMiddleware, isAdmin, deleteAProduct);//delete a product
 
-
 // modifications for web ///
-const { isAuthenticated } = require('../../middlewares/authMiddleware');
-const Product = require('../../models/productModel');
-const Cart = require('../../models/cartModel');
+const { isAuthenticated } = require("../../middlewares/authMiddleware");
+const Product = require("../../models/productModel");
+const Cart = require("../../models/cartModel");
 
 //get get all products using UI
 router.get("/", async (req, res) => {
     try {
-        const allProducts = await getAllProducts(req, res); // Fetch all products
-        res.render("pages/home", {
-            title: "Home",
-            products: allProducts, // Pass products to the view
-            layout: 'layouts/main' // Specify the layout to use
-        });
+      const allProducts = await getAllProducts(req, res); // Fetch all products
+      res.render("pages/home", {
+        title: "Home",
+        products: allProducts, // Pass products to the view
+        layout: "layouts/main", // Specify the layout to use
+      });
     } catch (error) {
-        console.error(error);
-        res.status(500).send("Server Error");
+      console.error(error);
+      res.status(500).send("Server Error");
     }
-});//get get all products using UI
-router.get("/products/", async (req, res) => {
+  });
+
+  router.get("/products", async (req, res) => {
     try {
-        const allProducts = await getAllProducts(req, res); // Fetch all products
-        res.render("pages/products", {
-            title: "Products",
-            products: allProducts, // Pass products to the view
-            layout: 'layouts/main' // Specify the layout to use
-        });
+      const allProducts = await getAllProducts(req, res); // Fetch all products
+      res.render("pages/products", {
+        title: "Products",
+        products: allProducts, // Pass products to the view
+        layout: "layouts/main", // Specify the layout to use
+      });
     } catch (error) {
-        console.error(error);
-        res.status(500).send("Server Error");
+      console.error(error);
+      res.status(500).send("Server Error");
     }
-});
+  });
+
 
 // modifications for web ///
 
